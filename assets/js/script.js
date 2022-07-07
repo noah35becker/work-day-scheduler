48a5b029a1a6426dd3bf4636553c67eedbe4eb5b
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-07 14:48:09 -0400

    localStorage functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,64 @@
 
 const DateTime = luxon.DateTime;
 
+var events = JSON.parse(localStorage.getItem('events')) || [];
+
+
 // Set text of current day
 $('#currentDay').text(DateTime.now().toFormat('cccc, LLLL d, y')); 
 
-// Establish past/present/future color-coding
-$('.row').each(function(){
-    var rowHour = $(this).find('.hour').text();
-    rowHour = rowHour === 'Noon' ? '12PM' : rowHour;
-    rowHour = +DateTime.fromFormat(rowHour, 'ha').toFormat('H'); // Returns rowHour in 24-hr time, coerced to a number
-    
-    var currentHour = +DateTime.now().toFormat('H'); //Returns currentHour in 24-hr time, coerced to a number
 
-    if (rowHour < currentHour)
-        $(this).children('.description-wrapper').addClass('past');
-    else if (rowHour == currentHour)
-        $(this).children('.description-wrapper').addClass('present');
-    else
-        $(this).children('.description-wrapper').addClass('future');
-});
+// Establish past/present/future color-coding
+function colorCoding(){
+    $('.row').each(function(){
+
+        // Get rowHour in 24-hr time (coerced to a number)
+        var rowHour = $(this).find('.hour').text();
+        rowHour = rowHour === 'Noon' ? '12PM' : rowHour;
+        rowHour = +DateTime.fromFormat(rowHour, 'ha').toFormat('H');
+        
+        // Get currentHour in 24-hr time (coerced to a number)
+        var currentHour = +DateTime.now().toFormat('H');
+        
+        //Color-code
+        if (rowHour < currentHour)
+            $(this).children('.description-wrapper').addClass('past');
+        else if (rowHour == currentHour)
+            $(this).children('.description-wrapper').addClass('present');
+        else
+            $(this).children('.description-wrapper').addClass('future');
+    });
+}
+
+
+// Save events to localStorage
+function saveEvents(){
+    events = [];
+    
+    $('.row').each(function (){
+        events.push({
+            hour: $(this).find('.hour').text(),
+            description: $(this).find('.description').text()
+        });
+    });
+
+    localStorage.setItem('events', JSON.stringify(events));
+}
+
+
+// Populate event descriptions on page
+function populateDescriptions(){
+    events.forEach(elem => {
+        $('.hour:contains("' + elem.hour + '")')
+            .closest('.row')
+            .find('.description')
+            .text(elem.description);
+    });
+}
+
+
+
+// INITIALIZE PAGE
+populateDescriptions();
+colorCoding();
+// Add timers for refreshing current hour / current day
