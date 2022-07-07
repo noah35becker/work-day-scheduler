8bc5ff154f0faf11079a3f8d4d2187f579ce90a5
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-07 14:15:14 -0400

    past/present/future color-coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 
 const DateTime = luxon.DateTime;
 
+// Set text of current day
 $('#currentDay').text(DateTime.now().toFormat('cccc, LLLL d, y')); 
+
+// Establish past/present/future color-coding
+$('.row').each(function(){
+    var rowHour = $(this).find('.hour').text();
+    rowHour = rowHour === 'Noon' ? '12PM' : rowHour;
+    rowHour = +DateTime.fromFormat(rowHour, 'ha').toFormat('H'); // Returns rowHour in 24-hr time, coerced to a number
+    
+    var currentHour = +DateTime.now().toFormat('H'); //Returns currentHour in 24-hr time, coerced to a number
+
+    if (rowHour < currentHour)
+        $(this).children('.description-wrapper').addClass('past');
+    else if (rowHour == currentHour)
+        $(this).children('.description-wrapper').addClass('present');
+    else
+        $(this).children('.description-wrapper').addClass('future');
+});
