b7225dcdee7a3374a417a229f1f0d17810924944
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-07 14:52:25 -0400

    reorganizing js, updating header formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,20 @@ var events = JSON.parse(localStorage.getItem('events')) || [];
 
 
 // Set text of current day
-$('#currentDay').text(DateTime.now().toFormat('cccc, LLLL d, y')); 
+function setToday(){
+    $('#currentDay').text(DateTime.now().toFormat('cccc, LLLL d, y')); 
+}
+
+
+// Populate event descriptions on page
+function populateDescriptions(){
+    events.forEach(elem => {
+        $('.hour:contains("' + elem.hour + '")')
+            .closest('.row')
+            .find('.description')
+            .text(elem.description);
+    });
+}
 
 
 // Establish past/present/future color-coding
@@ -20,7 +33,7 @@ function colorCoding(){
         // Get currentHour in 24-hr time (coerced to a number)
         var currentHour = +DateTime.now().toFormat('H');
         
-        //Color-code
+        //Add color-coded classes
         if (rowHour < currentHour)
             $(this).children('.description-wrapper').addClass('past');
         else if (rowHour == currentHour)
@@ -46,19 +59,9 @@ function saveEvents(){
 }
 
 
-// Populate event descriptions on page
-function populateDescriptions(){
-    events.forEach(elem => {
-        $('.hour:contains("' + elem.hour + '")')
-            .closest('.row')
-            .find('.description')
-            .text(elem.description);
-    });
-}
-
-
 
 // INITIALIZE PAGE
+setToday();
 populateDescriptions();
 colorCoding();
 // Add timers for refreshing current hour / current day
